f67aba6cc60516de5b408c4aec9acadb94b40f59
Rajat Singh <s.rajat55@gmail.com>
2026-06-17 12:23:07 +0530

    added astro-indexnow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,18 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import indexnow from 'astro-indexnow';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.cloudyunicorn.com',
-  integrations: [sitemap()],
+  integrations: [
+    sitemap(),
+    indexnow({
+      key: '8521d9be856644f192b0c5cb2cb7ffc7',
+      enabled: true,
+    }),
+  ],
   server: {
     host: true
   },
